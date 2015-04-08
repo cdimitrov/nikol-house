31c7fbbe045450b3f96eb1e4196abfaaffcd894a
BoDimitrova <bogdana_dimitrova@abv.bg>
2015-04-08 11:24:57 +0300

    Custom.js refactoring
    Created handler functions;
    Removed unused old code.

--- sunshine/js/custom.js
+++ sunshine/js/custom.js
@@ -11,20 +11,8 @@ jQuery(document).ready(function() {
 	});
 
 
-
 	// ==================================================
-	// lazyload ????
-	// ==================================================
-	 $(function() {
-          $("img").lazyload({
-              effect : "fadeIn",
-			  effectspeed: 900
-          });
-      });
-
-
-	// ==================================================
-	// Gallery test
+	// Gallery slider with thumbnails
 	// ==================================================
 	
 	$(function() {
@@ -36,23 +24,39 @@ jQuery(document).ready(function() {
 		interval: 0
 		});
 		
-		// handles the carousel thumbnails
+		selectGalleryThumb_handler();
+		autoslideGalleryPhoto_handler();
+		
+	});
+	
+	// Handles the thumbnails carousel selection
+	function selectGalleryThumb_handler() {
 		$('[id^=carousel-selector-]').click( function(){
 		  var id_selector = $(this).attr("id");
 		  var id = id_selector.replace('carousel-selector-','');
 		  id = parseInt(id);
+		  
+		  // Navigate to the selected from thumb carousel photo-item
 		  $('#myCarousel').carousel(id);
+		  
+		  // Update the photo carousel
 		  $('[id^=carousel-selector-]').removeClass('selected');
 		  $(this).addClass('selected');
 		  
+		  // Update the thumb carousel active page
 		  $('.thumb-page').removeClass('active');
 		  $(this).parents('.thumb-page').addClass('active');
 		});
-
-		// when the carousel slides, auto update
+	}
+	
+	// Handles the photo carousel slide event, auto update the thumb carousel
+	function autoslideGalleryPhoto_handler() {
 		$('#myCarousel').on('slide.bs.carousel', function (e) {
-		  var id = $('.item.active.photo-item').data('slide-number');
-		  id = parseInt(id)+1;
+		  var photoId = $('.item.active.photo-item').data('slide-number');
+		  
+		  // Prepare the thumb carousel for the photo carousel auto slide
+		  // Set the selected thumb ID to be the next one 
+		  var id = parseInt(photoId) + 1;
 		  if($('[id^=carousel-selector-]').length <= id)
 		  {
 			id = 0;
@@ -60,12 +64,13 @@ jQuery(document).ready(function() {
 		  $('[id^=carousel-selector-]').removeClass('selected');
 		  $('[id=carousel-selector-'+id+']').addClass('selected');
 		  
+		  // Set the selected thumb page and auto slide to it
 		  var currentThumbPage = $('[id=carousel-selector-'+id+']').parents('.thumb-page');
 		  $('.thumb-page').removeClass('active');
 		  currentThumbPage.addClass('active');
 		  var currentThumbPageIndex = parseInt(currentThumbPage.data('thumb-page'));
 		  $('#myCarouselThumbs').carousel(currentThumbPageIndex);
 		});
-	});
+	}
 
 });
