a84510e93c1c5a5643915bc6ea6c4071f80c5ab4
BoDimitrova <bogdana_dimitrova@abv.bg>
2015-05-25 00:26:02 +0300

    Version 1 of Email

--- sunshine/js/contact.js
+++ sunshine/js/contact.js
@@ -8,81 +8,51 @@ $(document).ready(function() {
 
         var error = false;
 
-		if(email.length == 0 || email.indexOf("@") == "-1" || email.indexOf(".") == "-1") {
-		   var error = true;
-		   $("#error_email").fadeIn(500);
-		}
-		else {
-		   $("#error_email").fadeOut(500);
-		}
-		
-		if(message.length == 0) {
-			var error = true;
-			$("#error_message").fadeIn(500);
-		}
-		else {
-			$("#error_message").fadeOut(500);
-		}
-         
+	//validation
+	if(email.length == 0 || email.indexOf("@") == "-1" || email.indexOf(".") == "-1") {
+	   var error = true;
+	   $("#error_email").fadeIn(500);
+	}
+	else {
+	   $("#error_email").fadeOut(500);
+	}
+	
+	if(message.length == 0) {
+		var error = true;
+		$("#error_message").fadeIn(500);
+	}
+	else {
+		$("#error_message").fadeOut(500);
+	}
+        
+        //all fields are correct, then send mail 
         if(error == false) {
            //$("#send").attr({"disabled" : "true", "value" : "Изпращане..." });
-            
-/*            $.ajax({
-             type: "POST",
-             url : "https://api:key-29aa5438836eef41f32815976693a128@api.mailgun.net/v3/mg.nikol-house.com/messages",    
-             data: { from: name + "<" + email + ">",  to:"info@nikol-house.com", subject:subject, text:message},
-			 //without this xhrFields withCredentials shit it doesn't work!!!
-			 xhrFields: {
-				withCredentials: true
-			 },
-			 crossDomain: true,
-             success: function(data){    
-              if(data == 'success'){
-				alert("success: " + data.responseText);
-                $("#btnsubmit").remove();
-                $("#mail_success").fadeIn(500);
-              }else{
-                $("#mail_failed").html(data).fadeIn(500);
-                $("#send").removeAttr("disabled").attr("value", "Изпрати");
-              }     
-             },
-			error: function(xhr, status, e) {
-			console.log(xhr);
-			console.log(status);
-			console.log(e);
-			alert("error: ");
-			}			
-           }); */ 
-		   var test = function (data) {
-			alert(data);
-		   };
+           
            $.ajax({
-             type: "GET",
-             url : "https://api:key-29aa5438836eef41f32815976693a128@api.mailgun.net/v3/mg.nikol-house.com/messages",    
-             data: { from: name + "<" + email + ">",  to:"info@nikol-house.com", subject:subject, text:message},
-			 dataType: "jsonp",
-			 crossDomain: true,
-			 jsonpCallback: "test",
-             success: function(data){    
-              if(data == 'success'){
-				alert("success: " + data.responseText);
-                $("#btnsubmit").remove();
-                $("#mail_success").fadeIn(500);
-              }else{
-                $("#mail_failed").html(data).fadeIn(500);
-                $("#send").removeAttr("disabled").attr("value", "Изпрати");
-              }     
+             type: "POST",
+             url : "./send.php",    
+             data: "name=" + name + "&email=" + email + "&subject=" + subject + "&message=" + message,
+             success: function(data){  
+	              if(data == 'success'){
+	                console.log(data);
+	                $("#btnsubmit").remove();
+	                $("#mail_success").fadeIn(500);
+              		}else{
+              		console.log(data);
+	                $("#mail_failed").html(data).fadeIn(500);
+	                $("#send").removeAttr("disabled").attr("value", "Изпрати");
+              }
              },
-			error: function(xhr, status, e) {
-			console.log(xhr);
-			console.log(status);
-			console.log(e);
-			alert("error: ");
-			}			
-           });		   
+		error: function(e) {
+		alert("Failed to send request.");
+		console.log(e);
+		}
+           }); 
+             
         }
 		    
-		return false;                      
+	return false;                      
     }); 
 
 	
@@ -106,52 +76,3 @@ $(document).ready(function() {
 		}
 	});
 });
-
- 
-     /* $(document).ready(function(){
-      $("#contact").submit(function(){
-        var name   = $("#name").val();
-        var email  = $("#email").val();
-        var message  = $("#message").val();
-
-        var error = false;
-
-         if(email.length == 0 || email.indexOf("@") == "-1" || email.indexOf(".") == "-1"){
-           var error = true;
-           $("#error_email").fadeIn(500);
-         }else{
-           $("#error_email").fadeOut(500);
-         }
-         if(message.length == 0){
-            var error = true;
-            $("#error_message").fadeIn(500);
-         }else{
-            $("#error_message").fadeOut(500);
-         }
-         
-         if(error == false){
-           //$("#send").attr({"disabled" : "true", "value" : "Loading..." });
-            
-           $.ajax({
-             type: "POST",
-             url : "./send.php",    
-             data: "name=" + name + "&email=" + email + "&subject=" + "You Got Email" + "&message=" + message,
-             success: function(data){  
-				alert("test");
-              if(data == 'success'){
-                $("#btnsubmit").remove();
-                $("#mail_success").fadeIn(500);
-              }else{
-                $("#mail_failed").html(data).fadeIn(500);
-                $("#send").removeAttr("disabled").attr("value", "Изпрати");
-              }
-             },
-							error: function(e) {
-			alert("fuck it");
-			alert(e.responseText);
-			}
-           });  
-        }
-		    return false;                      
-      });    
-    }); */
--- sunshine/send.php
+++ sunshine/send.php
@@ -1,10 +1,12 @@
 <?php
 if($_POST){
     $name = $_POST['name'];
+    $subject= $_POST['subject'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-
-//send email
-    mail("icyflower89@abv.bg", $name, $message);
+    $headers = 'From: ' . $name . ' <' . $email . '>';
+    //send email
+    mail("chvdimitrov@gmail.com", $subject, $message, $headers);
+    mail("info@nikol-house.com", $subject, $message, $headers);
 }
 ?>
