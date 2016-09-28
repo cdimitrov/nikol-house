07927cc748cfb6d24a2803248cd18c081202706c
BoDimitrova <bogdana_dimitrova@abv.bg>
2016-09-28 13:48:29 +0300

    Update send.php to work with UTF-8
    UTF-8 needed

--- sunshine/send.php
+++ sunshine/send.php
@@ -4,9 +4,17 @@ if($_POST){
     $subject= $_POST['subject'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $headers = 'From: ' . $name . ' <' . $email . '>';
+
+    $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
+    $name= "=?UTF-8?B?".base64_encode($name)."?=";
+    $from =  "$name <$email>";
+      
+    $headers   = array();
+    $headers[] = "From: $from";
+    $headers[] =  "Content-type: text/html; charset=UTF-8"; 
+
     //send email
-    mail("chvdimitrov@gmail.com", $subject, $message, $headers);
-    mail("info@nikol-house.com", $subject, $message, $headers);
+    mail("chvdimitrov@gmail.com", $subject, $message,  implode("\r\n", $headers));
+    mail("info@nikol-house.com", $subject, $message,  implode("\r\n", $headers));
 }
 ?>
